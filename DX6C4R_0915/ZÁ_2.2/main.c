a3bac4989081ab80eb0ec822c92e972c7e2a1bb9
Ákos Zarándi <79195687+Zakos100@users.noreply.github.com>
2021-10-27 10:56:51 +0200

    Add files via upload

---
+++ DX6C4R_0915/ZÁ_2.2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    pelda_1();
+    return 0;
+}
+
+void pelda_1() {
+FILE *fp;
+char ch;
+char fnev[50];
+    printf("Filenev: ");
+    scanf("%s", fnev);
+    fp=fopen (fnev, "w");
+    printf("Uzenet:");
+    while ((ch=getchar())!='#'){
+        putc(ch,fp);
+
+    }
+    fclose(fp);
+
+    fp=fopen(fnev, 'r');
+    while ((ch=getc(fp))!=EOF){
+
+        printf("%c", ch);
+    }
+
+    fclose(fp);
+    return 0;
+
+
+
+
+
+
+
+}
